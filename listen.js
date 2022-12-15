ef2480db223af4453e05a83d7d728d3f93780261
plaintree <cheukhei04@hotmail.com>
2022-12-15 13:19:38 +0000

    reconfigure connection.js for production deployment

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const port = 3000;
+const { PORT = 3000 } = process.env;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
